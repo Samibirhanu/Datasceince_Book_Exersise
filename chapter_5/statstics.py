3faa2f8184d1a92b24bc48808f68ee85a8029f60
samuel <84203477+Samibirhanu@users.noreply.github.com>
2024-10-25 10:36:04 +0300

    python statstics with range variance and interquartile range

--- chapter_5/statstics.py
+++ chapter_5/statstics.py
@@ -50,3 +50,73 @@ def mode(x: List[float]) -> List[float]:
     max_count = max(counts.values())
     return [x_i for x_i, count in counts.items() if count == max_count]
 print(mode([1,9,3,4,3,3,4,6,77,6,96,9,4,9,4,3,0,0]))
+
+
+def quntile(xs: List[float], p: float) -> float:
+    """Returns the pth-percentile value in x"""
+    p_index = int(p *len(xs))
+    return sorted(xs)[p_index]
+quntile(num_friends, 0.10) == 25
+
+# measures of spread  Range
+def data_range(r: List[float]) -> float:
+    """Returns the range value of list"""
+    sorted_range = sorted(r)
+    return (sorted_range[-1] - sorted_range[0])
+assert data_range([1, 2, 3, 4, 5, 6]) == 5, 'your function is not correct'
+
+
+# variance
+def variance(v: List[float]) -> float:
+    """Returns the variance of data list my trial"""
+    mean_v = mean(v)                           # find mean
+    # print(mean_v)
+    dist_mean = [(i - mean_v)**2 for i in v]   # find distance between the mean from all data points
+    n = len(dist_mean)
+    variance = sum(dist_mean)/ (n - 1)
+    return variance
+sample = variance([1, 2, 3, 4, 5])
+print(sample)
+
+Vector = List[List[float]]
+
+def dot(v:Vector, w:Vector) -> float:
+    """Computes v_1 * w_1 + ... + v_n * W_n"""
+    assert len(v) == len(w), "vectors must be same length"
+
+    return sum(v_i * w_i for v_i, w_i in zip(v, w))
+
+def sum_of_squares(v: Vector) -> float:
+    """Returns v_1 * v_1 + ... + v_n * v_n"""
+    return  dot(v, v)
+
+def de_mean(xs: List[float]) -> List[float]:
+    """Translate xs by substracting its mean (so the result has mean 0)"""
+    x_bar = mean(xs)
+    return [x -x_bar for x in xs ]
+
+def variance_joel(xs: List[float] ) -> float:
+    """Almost the averaage squared deviation from the mean"""
+    assert len(xs) >= 2, "variance requirs at least two elements"
+
+    n = len(xs)
+    deviations = de_mean(xs)
+    return sum_of_squares(deviations) / (n - 1)
+# assert variance_joel([1, 2, 3, 4, 5]) == 2
+
+print(variance([2.74, 1.23, 2.63, 2.22, 3, 1.98]))
+print(variance_joel([2.74, 1.23, 2.63, 2.22, 3, 1.98]))
+
+import math
+def standard_deviation(xs: List[float]) -> float:
+    """The standard deviation is the square root of the variance"""
+    return math.sqrt(variance_joel(xs))
+
+# print(standard_deviation([1, 2, 3, 4, 5])) 
+
+# interquartile_range 
+def interquartile_range(xs: List[float]) -> float:
+    """Returns the difference between the 75%-ile and the 25%-tile"""
+    return quntile(xs, 0.75) - quntile(xs, 0.25)
+
+print(interquartile_range([1,2,3,4,5,6,7,8,9]))
